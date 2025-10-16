8446dd1cd5735591a6e1ce5b37b294d33db99b9c
jayellho <jayellho@gmail.com>
2025-10-16 19:43:29 -0400

    feat: completed 3-39.py.

--- 3_data-structures/interview_problems/3-39.py
+++ 3_data-structures/interview_problems/3-39.py
@@ -1,4 +1,64 @@
 '''
 Implement an algorithm to reverse a linked list. Now do it without recursion.
 '''
-# TODO
+# Helper classes and funtions.
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+def build_linked_list(size):
+    head = Node(None)
+    ptr = head
+    for i in range(size):
+        to_insert = Node(i)
+        ptr.next = to_insert
+        ptr = ptr.next
+
+    return head.next
+
+def print_linked_list(head):
+    print("Linked list values:")
+
+    ptr = head
+
+    while ptr:
+        print(ptr.val)
+        ptr = ptr.next
+
+
+# Iterative implemntation.
+def reverse_ll_iterative(head):
+    ptr = head
+    prev = None
+
+
+    while ptr:
+
+        orig_next = ptr.next
+        ptr.next = prev
+        prev = ptr
+        ptr = orig_next
+    
+
+    return prev
+
+
+
+# Driver code.
+if __name__ == "__main__":
+
+    # build linked list.
+    eg1 = build_linked_list(6)
+
+    print_linked_list(eg1)
+
+    reversed_eg1 = reverse_ll_iterative(eg1)
+
+    print_linked_list(reversed_eg1)
+
+
+
+
+
+    
