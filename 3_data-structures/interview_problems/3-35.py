aa5fccc2eb98410612be7f9df6f0a89191ac5ffc
jayellho <jayellho@gmail.com>
2025-10-16 19:49:26 -0400

    feat: completed 3-35.py.

--- 3_data-structures/interview_problems/3-35.py
+++ 3_data-structures/interview_problems/3-35.py
@@ -1,5 +1,6 @@
 '''
 [3] Imagine you have a closet full of shirts. What can you do to organize your shirts for easy retrieval?
-'''
 
-# TODO
+We can try to sort it by some property (maybe color?), and then perform binary search since it is now sorted.
+The sorting should take O(n log n) time complexity and the search would take O(log n). The overall time complexity is O(n log n).
+'''
