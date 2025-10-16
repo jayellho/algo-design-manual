6fffa9ca8f532934a85c7973dc300c47ecc9b785
jayellho <jayellho@gmail.com>
2025-10-16 19:46:34 -0400

    feat: completed 3-34.py.

--- 3_data-structures/interview_problems/3-34.py
+++ 3_data-structures/interview_problems/3-34.py
@@ -1,5 +1,9 @@
 '''
 [3] What method would you use to look up a word in a dictionary?
-'''
 
-# TODO
+My answer:
+
+A dictionary in the traditional sense consists of words sorted alphabetically.
+Given a sorted list of these words as in a dictionary, I would conduct binary search to get the word I need.
+This would be an O(log n) operation, where n is the number of words in the dictionary.
+'''
