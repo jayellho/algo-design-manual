14c4d32df413c9ed7aead7b0e96e9bf138fb39e4
jayellho <jayellho@gmail.com>
2025-10-16 20:02:40 -0400

    feat: completed 3-36.py.

--- 3_data-structures/interview_problems/3-36.py
+++ 3_data-structures/interview_problems/3-36.py
@@ -2,3 +2,77 @@
 [4] Write a function to find the middle node of a singly linked list.
 '''
 # TODO
+'''
+thought:
+- create 2 pointers.
+    - 1 will be 2x speed of the other.
+- odd e.g. LL of length 7.
+        fast -1 1 3 5 break
+        slow 0  1 2 3 break
+
+- even e.g. LL of length 8.
+        fast -1 1 3 5 7 break
+        slow 0  1 2 3 4 break
+
+- NOTE: my implemntation takes the 2nd value as the middle for even-lengthed lists.
+'''
+
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node(None)
+
+    # builds a linked list of self.size with arbitrarily increasing numbers.
+    def build_list(self,size):
+        ptr = self.head
+        for i in range(size):
+            to_insert = Node(i)
+            ptr.next = to_insert
+            ptr = ptr.next
+        self.head = self.head.next
+    
+    def print_list(self):
+        print("Linked list values: ")
+        ptr = self.head
+        
+        while ptr:
+            print(ptr.val)
+            ptr = ptr.next
+
+
+def get_middle_node(head):
+
+    ptr = head
+    
+    slow = ptr
+    fast = ptr
+
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    print(f"middle val is {slow.val}")
+    return slow
+
+# Driver code.
+if __name__ == "__main__":
+
+    # create singly linked list example.
+    eg1 = LinkedList()
+    eg1.build_list(6)
+    eg1.print_list()
+
+    get_middle_node(eg1.head)
+
+    eg1.build_list(5)
+    eg1.print_list()
+    get_middle_node(eg1.head)
+
+    eg1.build_list(9)
+    eg1.print_list()
+    get_middle_node(eg1.head)
